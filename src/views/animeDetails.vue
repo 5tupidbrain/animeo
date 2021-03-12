8cfd248c635a788ab0e50144bb08e9b30e16211c
Saransh Hardaha <hellosaranshh@gmail.com>
2021-03-12 08:58:52 +0530

    animeDetails Update

--- src/views/animeDetails.vue
+++ src/views/animeDetails.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="container my-5">
     <div v-if="animeDet" class="d-flex flex-column">
-      <div class="my-4 d-flex flex-row">
+      <div class=" animeInfo my-4 d-flex">
         <div>
           <img class="image" :src="`${animeDet.img_url}`" alt="" />
         </div>
-        <div class="details my-auto mx-5">
+        <div class="details my-auto mx-md-5">
           <h2 class="display-4 font-weight-bold">{{ animeDet.name }}</h2>
           <p class=""><b>Summary: &nbsp; </b> {{ animeDet.about }}</p>
         </div>
       </div>
       <div class="episodes_link my-4">
         <h5 class="text-left">Episodes</h5>
-        <div class="episode_list">
-          <router-link
-            :to="{
-              name: 'animeEp',
-              params: {
-                animeName: anime_id,
-                animeEpisode: item,
-              },
-            }"
-            v-for="(item, index) in animeDet.episode_id"
-            :key="index"
-            class="epItem"
-          >
-            Episode {{ index + 1 }}
-          </router-link>
-        </div>
+        <ul class="episode_list">
+          <li v-for="(item, index) in animeDet.episode_id" :key="index">
+            <router-link
+              :to="{
+                name: 'animeEp',
+                params: {
+                  animeName: anime_id,
+                  animeEpisode: item,
+                },
+              }"
+              class="epItem"
+            >
+              Episode {{ index + 1 }}
+            </router-link>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -77,13 +77,23 @@ img {
   text-align: left;
   width: 100%;
 }
+.animeInfo {
+  flex-direction: row;
+}
 .episodes_link {
   display: flex;
   flex-direction: column;
   gap: 8px;
-  text-align: left;
+  text-align: center;
 }
-.episodes_link div a {
+.episode_list {
+  justify-content: center ;
+  display: flex !important;
+  flex-wrap: wrap ;
+  list-style: none ;
+  padding: 0 0 !important;
+}
+.episodes_link ul li a {
   text-decoration: none;
   color: white;
 }
@@ -93,5 +103,16 @@ img {
   background: rgba(255, 255, 255, 0.15);
   margin: 5px;
   display: inline-block;
+}
+@media screen and (max-width: 786px) {
+  .animeInfo {
+    flex-direction: column;
+  }
+  .details {
+    text-align: justify !important;
+    padding: 0 18px !important;
+    margin: 48px 0 0 0 !important;
+    width: 100%;
+  }
 }
 </style>
